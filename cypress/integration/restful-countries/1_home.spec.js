b3ebc96f3409a2561207a929a8c3761800b07594
Venkatasubramanian <pvenkat.rv@gmail.com>
2020-07-15 21:48:27 +0530

    restful-countries

---
+++ cypress/integration/restful-countries/1_home.spec.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+// https://new-york-times-api.netlify.app/#
+// https://newyork-times-api.netlify.app/
+
+context("Window", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("cy.window() - get the global window object", () => {
+    cy.window()
+      .should("have.property", "top")
+      .screenshot(`Home page - ${Cypress.config().baseUrl}/`);
+  });
+
+  // checks for the charset UTF-8
+  it("cy.document() - get the document object", () => {
+    cy.document().should("have.property", "charset").and("eq", "UTF-8");
+  });
+
+  // //matches with regex
+  it("cy.title() -2- get the title", () => {
+    cy.title().should("match", /[A-Za-z]\w+/);
+  });
+
+  // // specific title
+  // it("cy.title() -1- get the title", () => {
+  //   cy.title().should("include", "Countries");
+  // });
+
+  // it("cy.title() -2- get the title", () => {
+  //   cy.title().should("include", "Restful countries");
+  // });
+});
